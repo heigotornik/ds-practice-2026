9e7a2ae4796dd1bf228e138ca0be1f62e9656618
HMaandi <henrimaandi@gmail.com>
2026-02-18 11:46:54 +0200

    initial suggestion stuff

--- suggestions/src/app.py
+++ suggestions/src/app.py
@@ -5,25 +5,25 @@ import os
 # The path of the stubs is relative to the current file, or absolute inside the container.
 # Change these lines only if strictly needed.
 FILE = __file__ if '__file__' in globals() else os.getenv("PYTHONFILE", "")
-fraud_detection_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/fraud_detection'))
-sys.path.insert(0, fraud_detection_grpc_path)
-import fraud_detection_pb2 as fraud_detection
-import fraud_detection_pb2_grpc as fraud_detection_grpc
+suggestion_grpc_path = os.path.abspath(os.path.join(FILE, '../../../utils/pb/suggestion'))
+sys.path.insert(0, suggestion_grpc_path)
+import suggestion_pb2 as suggestion
+import suggestion_grpc as suggestion_grpc
 
 import grpc
 from concurrent import futures
 
 # Create a class to define the server functions, derived from
 # fraud_detection_pb2_grpc.HelloServiceServicer
-class HelloService(fraud_detection_grpc.HelloServiceServicer):
+class SuggestionService(suggestion_grpc.SuggestionServiceServicer):
     # Create an RPC function to say hello
-    def SayHello(self, request, context):
+    def SuggestBooks(self, request, context):
         # Create a HelloResponse object
-        response = fraud_detection.HelloResponse()
+        inputBooks = request.books
+        testbook1 = suggestion.Book()
+        response = suggestion.BookList( {testbook1} ) 
         # Set the greeting field of the response object
-        response.greeting = "Hello, " + request.name
         # Print the greeting message
-        print(response.greeting)
         # Return the response object
         return response
 
@@ -31,9 +31,9 @@ def serve():
     # Create a gRPC server
     server = grpc.server(futures.ThreadPoolExecutor())
     # Add HelloService
-    fraud_detection_grpc.add_HelloServiceServicer_to_server(HelloService(), server)
+    suggestion_grpc.add_SuggestionServiceServicer_to_server(SuggestionService(), server)
     # Listen on port 50051
-    port = "50051"
+    port = "50053"
     server.add_insecure_port("[::]:" + port)
     # Start the server
     server.start()
